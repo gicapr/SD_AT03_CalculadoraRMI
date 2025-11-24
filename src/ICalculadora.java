ba24398371b20b2dbf42f4db02110361ade9625f
gicapr <132844359+gicapr@users.noreply.github.com>
2025-11-24 07:32:00 -0300

    First commit
    Updated ICalculadora interface to support additional operations and double type.

--- src/ICalculadora.java
+++ src/ICalculadora.java
@@ -1,1 +1,16 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+public interface ICalculadora extends Remote {
+    
+    // Evolução do projeto original: Alterado para double para suportar a operação de divisão e números reais
+    public double soma(double a, double b) throws RemoteException;
+
+    // Evolução do projeto original: Foram adicionadas as operações de subtração, multiplicação e divisão
+    public double subtrai(double a, double b) throws RemoteException;
+    public double multiplica(double a, double b) throws RemoteException;
+    public double divide(double a, double b) throws RemoteException;
+
+    // Evolução do projeto original: Adição do cálculo de expressões matemáticas
+    public double calcularExpressao(String expressao) throws RemoteException;
+}
